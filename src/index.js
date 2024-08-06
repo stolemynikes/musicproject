0a01bd16317b58647e222111ebc45b7b54928162
stolemynikes <107889379+stolemynikes@users.noreply.github.com>
2024-08-06 23:06:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
+//Render
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
